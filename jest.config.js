d6db9b969aee763962c9fbd47f04bf6188a91e7e
Luan Alves <devluanpaiva@gmail.com>
2025-06-19 11:21:37 -0300

    chore: configure Jest to transform pdfjs-dist

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,16 @@
 const nextJest = require("next/jest");
 
-/** @type {import('jest').Config} */
-const createJestConfig = nextJest({
-  dir: "./",
-});
+const createJestConfig = nextJest({ dir: "./" });
 
 const config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   preset: "ts-jest",
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx|mjs)$": "babel-jest",
+  },
+  transformIgnorePatterns: ["/node_modules/(?!(pdfjs-dist)/)"],
 };
 
 module.exports = createJestConfig(config);
